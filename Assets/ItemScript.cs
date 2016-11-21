b8aac482cc7a59b10543d5637497e3896f26bfed
Claes <claes.gustav.andersson@gmail.com>
2016-11-21 18:31:52 +0100

    unfinished population of multiplayer scene. Also some  weird things with plugins (should still branch, but eh)

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ItemScript : MonoBehaviour {
 
 	GameScript holder = null;
+	bool triggered = false;
 
 
 	public void setHolder( GameScript gs)
@@ -18,13 +19,19 @@ public class ItemScript : MonoBehaviour {
 
 	void OnTriggerEnter() 
 	{
+		triggered = true;
 		if(holder)
 			holder.IntersectTrue();
 	}
 
 	void OnTriggerExit() 
 	{
+		triggered = false;
 		if(holder)
 			holder.IntersectFalse();	
+	}
+	public bool isTriggered()
+	{
+		return triggered;
 	}
 }
